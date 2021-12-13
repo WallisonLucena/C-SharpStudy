a752e255b384d380dcd5610d614bf7a9cf00f7fa
WallisonLucena <wallisonh123@gmail.com>
2021-12-13 03:22:55 -0300

    Exercise 01 - Section 6.

---
+++ ExerciciosFixacaoSecao6/AluguelQuarto.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace ExercicioFixacao1Secao6
+{
+    internal class AluguelQuarto
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public int RoomNumber { get; private set; }
+
+        public AluguelQuarto(string name, string email, int roomNumber)
+        {
+            Name = name;
+            Email = email;
+            RoomNumber = roomNumber;
+        }
+
+        public override string ToString()
+        {
+            return "Room " + RoomNumber + ": " + Name + ", " + Email;
+        }
+    }
+}
---
+++ ExerciciosFixacaoSecao6/Program.cs
@@ -0,0 +1,36 @@
+﻿using ExercicioFixacao1Secao6;
+
+Console.WriteLine("How many rooms will be rented?");
+    int roomsRented = int.Parse(Console.ReadLine());
+
+AluguelQuarto[] rooms = new AluguelQuarto[10];
+
+
+
+for (int i = 0; i < roomsRented; i++)
+{
+    Console.WriteLine("Rent #:" + i);
+    Console.WriteLine("What's the name?");
+    string name = Console.ReadLine();
+
+    Console.WriteLine("And the Email?");
+    string email = Console.ReadLine();
+
+    Console.WriteLine("And the room number?");
+    int roomNumber = int.Parse(Console.ReadLine());
+
+    rooms[roomNumber] = new AluguelQuarto(name, email, roomNumber);
+
+}
+
+
+for (int i = 0; i < 10; i++)
+{
+
+    if(rooms[i] != null)
+    {
+        Console.WriteLine("Busy rooms:");
+        Console.WriteLine(rooms[i]);
+    }
+    
+}
